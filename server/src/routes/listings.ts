884243a31c059f91747494c91dadb57a0da8fb42
Adam Hodgkinson <hodgkinson.adam@gmail.com>
2021-06-19 20:12:42 +0100

    netlify builds

--- server/src/routes/listings.ts
+++ server/src/routes/listings.ts
@@ -2,7 +2,7 @@ import { Express, Request, Response } from "express";
 import { Sequelize } from "sequelize";
 
 export default (app: Express, db:Sequelize) => {
-  app.get('route/to/endpoint', (req: Request, res: Response) => {
+  app.get('/route/to/endpoint', (req: Request, res: Response) => {
     res.send();
   })
 }
